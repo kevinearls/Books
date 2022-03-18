4bd6f9b5152bfa4a64f06562d845c75c76c4dfa9
Kevin Earls <kearls@redhat.com>
2022-03-18 20:03:08 +0100

    Initial Commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Kevin Earls on 18/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
